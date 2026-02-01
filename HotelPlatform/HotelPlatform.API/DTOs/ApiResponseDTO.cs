a6d3372860dcc491b6b6cfb0831d5f3bfa8c303f
Omar Gomaa <mrjmh934@gmail.com>
2026-02-01 21:28:57 +0200

    add the Archticure

---
+++ HotelPlatform/HotelPlatform.API/DTOs/ApiResponseDTO.cs
@@ -0,0 +1,19 @@
+﻿namespace Base.API.DTOs
+{
+    public class ApiResponseDTO
+    {
+        public int StatusCode { get; set; }
+        public string Message { get; set; } = string.Empty;
+        public object Data { get; set; }
+        public string TraceId { get; set; }
+
+        public ApiResponseDTO() { }
+        public ApiResponseDTO(int statusCode, string message, object data = null, string traceId = null)
+        {
+            StatusCode = statusCode;
+            Message = message;
+            Data = data;
+            TraceId = traceId;
+        }
+    }
+}
